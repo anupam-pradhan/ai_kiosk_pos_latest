b9af3513f3f8a7505a9e7ef30e41f09209d0cc76
anupam-pradhan <anupampradhan161@gmail.com>
2026-05-18 13:16:03 +0530

    missing line in printers manager file added

--- android/app/src/main/kotlin/com/example/ai_kiosk_pos/printer/PrinterManager.kt
+++ android/app/src/main/kotlin/com/example/ai_kiosk_pos/printer/PrinterManager.kt
@@ -20,6 +20,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Date
